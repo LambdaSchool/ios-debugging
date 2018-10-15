622c9e77bf25ee2f6e454915fd8ff2265e846bc7
Farhan Farooqui <farhan@farooqui.net>
2018-10-15 17:12:49 -0400

    Fixes PUT operation

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -48,7 +48,7 @@ class EntryController {
     private func put(entry: Entry, completion: @escaping ((Error?) -> Void) = { _ in }) {
         
         let identifier = entry.identifier ?? UUID().uuidString
-        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathComponent("json")
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
         var request = URLRequest(url: requestURL)
         request.httpMethod = "PUT"
         
@@ -123,7 +123,7 @@ class EntryController {
                 return
             }
             
-            moc.perform {
+            moc.performAndWait {
                 do {
                     try moc.save()
                     completion(nil)
