298780ec0172efae02c77ec356446961bf274682
Lotanna <sugabelly@gmail.com>
2019-04-02 19:21:57 -0400

    Update DebuggingNotes.swift

--- JournalCoreData/DebuggingNotes.swift
+++ JournalCoreData/DebuggingNotes.swift
@@ -9,3 +9,8 @@
 //LIST OF BUGS FOUND
 
 //When a mood emoji is selected, it turns white and invisible
+//After saving an entry, when it is clicked on to modify it, fatal error occurs on line 50 - titleTextField.text = entry.title - in the Entry Detail Controller
+//In the Entry Controller - the request had a problem
+// in the entry encode function - the timestamp was the identifier
+//The fetch from server function wasn't called in the viewload
+//In the entry view controller prepare for segue - there was no destination for the view segue
