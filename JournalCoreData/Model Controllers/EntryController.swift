40a8b0abba77710be6db2497612c8e6ebbb5cc66
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-17 13:22:21 -0600

    Update baseURL with my firebase

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-72b8d.firebaseio.com/")!
 
 class EntryController {
     
