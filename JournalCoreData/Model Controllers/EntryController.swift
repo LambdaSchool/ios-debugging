a41e1831250f69e347d59560fc9ae2f3072baeb0
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-06-24 16:17:50 -0400

    changed Firebase URL to my firebaseURL.  Fixed identifier typo.  Fixed bug where app would crash when clicking on a cell to go to detail page.

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,9 +9,13 @@
 import Foundation
 import CoreData
 
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-37db2.firebaseio.com/")!
 
 class EntryController {
+    
+    init() {
+        fetchEntriesFromServer()
+    }
     
     func createEntry(with title: String, bodyText: String, mood: String) {
         
@@ -136,7 +140,7 @@ class EntryController {
         guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identfier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
         
         var result: Entry? = nil
         do {
