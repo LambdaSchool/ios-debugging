04bb682d91589fc73b993be311361c25732c8d3e
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-12-09 15:32:12 -0800

    Initial Commit

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,8 +9,8 @@
 import Foundation
 import CoreData
 
-#error("Change this value to your own firebase database! (and then delete this line)")
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+
+let baseURL = URL(string: "https://journal-5b2fb.firebaseio.com/")!
 
 class EntryController {
     
