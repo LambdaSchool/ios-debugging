31a1385f3fa41bf729dc10e1dd974211de73fef9
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-12-09 15:42:47 -0800

    Call fetchEntryFromServer from viewWillAppear Add viewDidLoad with fetchEntryFromServer Fixed Crash on launch with spelling error "identifier" in the NSPredicate

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -13,9 +13,14 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        entryController.fetchEntriesFromServer()
         tableView.reloadData()
     }
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            entryController.fetchEntriesFromServer()
+        }
     
     // MARK: - Table view data source
     
