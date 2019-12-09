b3cb78d3871b6e233f72c803e66d8e08149ec07a
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-09 13:47:47 -0700

    Fix baseURL to correct Firebase url; word 'identifier' in NSPredicate format was missing an 'i' in between the t and f, fixed.

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-let baseURL = URL(string: "https://coredatasprintchallenge.firebaseio.com/")!
+let baseURL = URL(string: "https://ios-9-journal-core-data.firebaseio.com/")!
 
 class EntryController {
     
@@ -136,7 +136,7 @@ class EntryController {
         guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identfier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
         
         var result: Entry? = nil
         do {
