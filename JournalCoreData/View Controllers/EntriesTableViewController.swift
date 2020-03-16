d3636bbbcf861eb98bbd7620ee94349bfb2d473a
Chris <chris.gonzales.82@gmail.com>
2020-03-16 13:11:22 -0700

    Fixed an issue where an entry did not pass to the Detail scene when the cell was tapped.

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import CoreData
 
 class EntriesTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         tableView.reloadData()
     }
     
