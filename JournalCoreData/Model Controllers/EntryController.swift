5e2c682f40f641c1b9fb926f6a7d35e0caa9fc0d
David Wright <davidtwright89@gmail.com>
2020-04-08 02:05:29 -0400

    Fixed bug where changes would not PUT to Firebase

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -17,7 +17,11 @@ class EntryController {
         
         let entry = Entry(title: title, bodyText: bodyText, mood: mood)
         
-        put(entry: entry)
+        put(entry: entry) { error in
+            if let error = error {
+                print(error)
+            }
+        }
         
         saveToPersistentStore()
     }
@@ -29,7 +33,11 @@ class EntryController {
         entry.timestamp = Date()
         entry.mood = mood
         
-        put(entry: entry)
+        put(entry: entry) { error in
+            if let error = error {
+                print(error)
+            }
+        }
         
         saveToPersistentStore()
     }
@@ -44,7 +52,7 @@ class EntryController {
     private func put(entry: Entry, completion: @escaping ((Error?) -> Void) = { _ in }) {
         
         let identifier = entry.identifier ?? UUID().uuidString
-        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathComponent("json")
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
         var request = URLRequest(url: requestURL)
         request.httpMethod = "PUT"
         
