9b7669c34842febbd8a228b3765855131fbc27d1
Christopher DeVito <toolofjesus@protonmail.com>
2020-04-13 16:22:45 -0400

    Intitial commit. Set up firebase.

--- JournalCoreData/View Controllers/EntryDetailViewController.swift
+++ JournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -41,14 +41,14 @@ class EntryDetailViewController: UIViewController {
     }
     
     private func updateViews() {
-        guard let entry = entry else {
-                title = "Create Entry"
+        guard let entry = entry, let title = entry.title, let body = entry.bodyText else {
+            self.title = "Create Entry"
                 return
         }
         
-        title = entry.title
-        titleTextField.text = entry.title
-        bodyTextView.text = entry.bodyText
+        self.title = entry.title
+        titleTextField.text = title
+        bodyTextView.text = body
         
         var segmentIndex = 0
         
