78be61990c06b60ad0a4f61982421911bc8fc478
Brad <braddiroff@icloud.com>
2020-04-13 16:34:14 -0400

    Started project, changed URL

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,8 +9,8 @@
 import Foundation
 import CoreData
 
-#error("Change this value to your own firebase database! (and then delete this line)")
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+
+let baseURL = URL(string: "https://newjournal-69bf2.firebaseio.com/")!
 
 class EntryController {
     
