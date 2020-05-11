b08b5fdeb392bde088c54419374da5fda89015f1
Chris Dobek <dobek.christopher@gmail.com>
2020-05-11 15:42:41 -0400

    Called FetchEntriesFromServer Method

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -14,6 +14,10 @@ let baseURL = URL(string: "https://debuggingjournal-3ab79.firebaseio.com/")!
 
 class EntryController {
     
+    init() {
+        fetchEntriesFromServer()
+    }
+    
     func createEntry(with title: String, bodyText: String, mood: String) {
         
         let entry = Entry(title: title, bodyText: bodyText, mood: mood)
@@ -129,6 +133,7 @@ class EntryController {
                     completion(error)
                 }
             }
+            completion(nil)
         }.resume()
     }
     
