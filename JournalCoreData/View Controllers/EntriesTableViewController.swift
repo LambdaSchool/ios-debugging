771cca0a003eb352c220405fb24393d745d5eaab
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-11 14:27:04 -0700

    Fixed the missing code in the segue switch statement in order to update the changes

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -36,6 +36,7 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath) as? EntryTableViewCell else { return UITableViewCell() }
         
         let entry = fetchedResultsController.object(at: indexPath)
+        
         cell.entry = entry
         
         return cell
@@ -114,6 +115,7 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
                 let indexPath = tableView.indexPathForSelectedRow else { return }
             
             destinationVC.entry = fetchedResultsController.object(at: indexPath)
+            destinationVC.entryController = entryController
             
         default:
             break
