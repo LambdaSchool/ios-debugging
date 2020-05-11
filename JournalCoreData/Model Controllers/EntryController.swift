994b0a6c26800bcaf7818c251f05a5373aff2867
Shawn James <shawn.james@me.com>
2020-05-11 18:40:48 -0400

    Add my own firebase link to the project

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -9,8 +9,7 @@
 import Foundation
 import CoreData
 
-#error("Change this value to your own firebase database! (and then delete this line)")
-let baseURL = URL(string: "https://journal-syncing.firebaseio.com/")!
+let baseURL = URL(string: "https://journal-d5a9d.firebaseio.com/")!
 
 class EntryController {
     
