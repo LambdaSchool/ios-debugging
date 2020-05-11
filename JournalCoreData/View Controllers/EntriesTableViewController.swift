dbf414dd532a9a6b04acffa66c0b535c68e2b529
Mark Poggi <markp@mac.com>
2020-05-11 14:05:17 -0700

    Fixed update entry bug by injecting dependency into the segue method

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -112,7 +112,8 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
         case "ViewEntry":
             guard let destinationVC = segue.destination as? EntryDetailViewController,
                 let indexPath = tableView.indexPathForSelectedRow else { return }
-            
+
+            destinationVC.entryController = entryController
             destinationVC.entry = fetchedResultsController.object(at: indexPath)
             
         default:
