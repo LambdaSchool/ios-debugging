13dda92ead4f3fb7ae8140e70bc8aa3f7b2a8e4a
Jarren Campos <jarrenmcampos@gmail.com>
2020-05-20 19:33:03 -0700

    Added firebase entrypoint

--- JournalCoreData/View Controllers/EntryDetailViewController.swift
+++ JournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -46,7 +46,7 @@ class EntryDetailViewController: UIViewController {
                 return
         }
         
-        title = entry.title
+        self.title = entry.title
         titleTextField.text = entry.title
         bodyTextView.text = entry.bodyText
         
