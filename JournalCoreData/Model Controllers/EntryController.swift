f5e286d750cb092d6142cc89fcc16eb539994f2e
Jarren Campos <jarrenmcampos@gmail.com>
2020-05-20 19:40:55 -0700

    Got detail view controller to populate

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -13,6 +13,10 @@ let baseURL = URL(string: "https://debuggin-project.firebaseio.com/")!
 
 class EntryController {
     
+    init() {
+        fetchEntriesFromServer()
+    }
+    
     func createEntry(with title: String, bodyText: String, mood: String) {
         
         let entry = Entry(title: title, bodyText: bodyText, mood: mood)
@@ -44,7 +48,7 @@ class EntryController {
     private func put(entry: Entry, completion: @escaping ((Error?) -> Void) = { _ in }) {
         
         let identifier = entry.identifier ?? UUID().uuidString
-        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathComponent("json")
+        let requestURL = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
         var request = URLRequest(url: requestURL)
         request.httpMethod = "PUT"
         
@@ -136,11 +140,12 @@ class EntryController {
         guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identfier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
         
         var result: Entry? = nil
         do {
-            result = try context.fetch(fetchRequest).first
+            let results = try context.fetch(fetchRequest)
+            result = results.first
         } catch {
             NSLog("Error fetching single entry: \(error)")
         }
@@ -170,7 +175,7 @@ class EntryController {
         entry.identifier = entryRep.identifier
     }
     
-    func saveToPersistentStore() {        
+    func saveToPersistentStore() {
         do {
             try CoreDataStack.shared.mainContext.save()
         } catch {
