d719593205c1e3f26302efe00526a8827697d7ee
Ronald Huston Jr <rghuston9@gmail.com>
2020-09-16 21:19:52 -0500

    et cetera

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -100,7 +100,6 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
     
     // MARK: - Navigation
     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         switch segue.identifier {
